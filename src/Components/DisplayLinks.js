4b422eb1f08db087fcf98778b52e3f2461aa3d5c
JohnDennehy <johndennehy101@gmail.com>
2021-02-03 22:32:46 +0000

    Added clipboard copy functionalityy, completed styling of links container section

--- src/Components/DisplayLinks.js
+++ src/Components/DisplayLinks.js
@@ -2,12 +2,16 @@ import React from 'react';
 import '../CSS/DisplayLinks.css'
 
 const DisplayLinks = (props) => {
+
+   const copyText = (e) => {
+navigator.clipboard.writeText(e.target.parentElement.childNodes[0].textContent)
+   }
    //console.log(props.originalLinks)
    let testArr = ['google.ie', 'google.com', 'google.de']
     return (
 <div className="linksParentContainer">
     {/* Should be props.originalLinks for both when api in play */}
-{testArr !== undefined ? testArr.map((link, i) => <div className="linksContainer"><p className="originalUrl" key={i}>{link}</p><div className="shortenedContainer"><p className="shortenedUrl" key={i}>{link}</p><button className="copyButton">Copy</button></div></div>): 'test'}
+{testArr !== undefined ? testArr.map((link, i) => <div className="linksContainer"><p className="originalUrl" key={i}>{link}</p><div className="shortenedContainer"><p className="shortenedUrl" key={i}>{link}</p><button className="copyButton" onClick={copyText}>Copy</button></div></div>): 'test'}
 </div>
     )
 }
