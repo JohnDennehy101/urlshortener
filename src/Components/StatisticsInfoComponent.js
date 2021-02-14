b170f24fb4bd4b2f9f7e34d83bafd3ad98a7544d
JohnDennehy <johndennehy101@gmail.com>
2021-02-14 22:30:51 +0000

    Finished desktop implementation of statistics component section

--- src/Components/StatisticsInfoComponent.js
+++ src/Components/StatisticsInfoComponent.js
@@ -2,18 +2,45 @@ import React from 'react';
 import '../CSS/StatisticsInfoComponent.css';
 
 const StatisticsInfoComponent = (props) => {
+    let parentClassName = 'sectionContainer'
+    let statisticsComponentParentContainer = 'statisticsComponentParentContainer'
+    
+    /*if (window.innerWidth < 500) {
+parentClassName = 'sectionContainer'
+
+statisticsComponentParentContainer = 'statisticsComponentParentContainer'
+    }
+    else if (window.innerWidth > 500 && props.index === '1') {
+        parentClassName = 'sectionContainer1'
+      
+        statisticsComponentParentContainer = 'statisticsComponentParentContainer1'
+    }
+     else if (window.innerWidth > 500 && props.index === '2') {
+        parentClassName = 'sectionContainer2'
+      
+        statisticsComponentParentContainer = 'statisticsComponentParentContainer2'
+    }
+     else if (window.innerWidth > 500 && props.index === '3') {
+        parentClassName = 'sectionContainer3'
+      
+        statisticsComponentParentContainer = 'statisticsComponentParentContainer3'
+    }*/
+
+    
     return (
-        <div>
-        <div className="statisticsComponentParentContainer">
-            <img src={props.iconPath} alt={props.altText} className="iconImage"/>
+        <div className={parentClassName}>
+            <div className={statisticsComponentParentContainer}>
+            <img src={props.iconPath} alt={props.altText} className='iconImage'/>
             <h2 className="statisticsComponentDetailTitle">{props.title}</h2>
             <p className="statisticsComponentDetailText">{props.detail}</p>
             
             
         </div>
+            
+       
         {
-            props.index !== '3' ?
- <div className="divider"></div> : ''
+          window.innerWidth < 500 && props.index !== '3' ?
+ <div className="divider"></div> : window.innerWidth > 500 && props.index !== '3' ? <div className="dividerDesktop"> </div> : ''
         }
        
         </div>
