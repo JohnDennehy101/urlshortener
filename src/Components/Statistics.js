c3eeac0bff23e05ef8981c1b765de8f454ed6b9b
JohnDennehy <johndennehy101@gmail.com>
2021-02-14 20:54:56 +0000

    Finished mobile styling of statistics section.

--- src/Components/Statistics.js
+++ src/Components/Statistics.js
@@ -5,12 +5,12 @@ import StatisticsInfoComponent from './StatisticsInfoComponent';
 const Statistics = () => {
     
     return (
-<div>
-    <h2>Advanced Statistics</h2>
-    <p>Track how your links are performing across the web with our advanced statistics dashboard.</p>
-    <StatisticsInfoComponent title="Brand Recognition" detail="Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instil confidence in your content." iconPath="../../images/icon-brand-recognition.svg" altText="Brand Recognition Icon"/>
-    <StatisticsInfoComponent title="Detailed Records" detail="Gain insight into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions." iconPath="../../images/icon-detailed-records.svg" altText="Detailed Records Icon"/>
-    <StatisticsInfoComponent title="Fully Customizable" detail="Improve brand awareness and content discoverability through customizable links, supercharging audience engagement." iconPath="../../images/icon-fully-customizable.svg" altText="Fully Customizable Icon"/>
+<div className="statisticsParentContainer">
+    <h2 className="statisticsHeader">Advanced Statistics</h2>
+    <p className="statisticsOverviewText">Track how your links are performing across the web with our advanced statistics dashboard.</p>
+    <StatisticsInfoComponent title="Brand Recognition" detail="Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instil confidence in your content." iconPath="../../images/icon-brand-recognition.svg" altText="Brand Recognition Icon" index="1"/>
+    <StatisticsInfoComponent title="Detailed Records" detail="Gain insight into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions." iconPath="../../images/icon-detailed-records.svg" altText="Detailed Records Icon" index="2"/>
+    <StatisticsInfoComponent title="Fully Customizable" detail="Improve brand awareness and content discoverability through customizable links, supercharging audience engagement." iconPath="../../images/icon-fully-customizable.svg" altText="Fully Customizable Icon" index="3"/>
    
 </div>
     )
--- src/Components/StatisticsInfoComponent.js
+++ src/Components/StatisticsInfoComponent.js
@@ -3,11 +3,19 @@ import '../CSS/StatisticsInfoComponent.css';
 
 const StatisticsInfoComponent = (props) => {
     return (
+        <div>
         <div className="statisticsComponentParentContainer">
             <img src={props.iconPath} alt={props.altText} className="iconImage"/>
             <h2 className="statisticsComponentDetailTitle">{props.title}</h2>
             <p className="statisticsComponentDetailText">{props.detail}</p>
             
+            
+        </div>
+        {
+            props.index !== '3' ?
+ <div className="divider"></div> : ''
+        }
+       
         </div>
     )
 }
